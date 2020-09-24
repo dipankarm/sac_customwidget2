f3f5f6ff2ea0e0416d1b5e4e291edcb2794dff43
shubhchamp <shubham.mishra24@gmail.com>
2020-09-24 10:52:51 +0000

    Change we component

--- webcomponent.js
+++ webcomponent.js
@@ -1,55 +1,55 @@
-(function()  {
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
+(function () {
+	let tmpl = document.createElement('template');
+	tmpl.innerHTML = `
         <h1>Hello World</h1>
-    `; // hardcoded template. instead of style tag h1 tag used 
+    `;
 
-    //com-sap-.. name of HTML TAG thats implemented . On Running the app on Devtool - this tag will be shown 
-    //"com-sap-sample-helloworld1"
-    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+	customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
 
-        // constructor is called once adnd standard webcompnent call back  , attach the templete to the shadow DOM  
 		constructor() {
-			super(); 
-			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+			super();
+			this._shadowRoot = this.attachShadow({
+				mode: "open"
+			});
+			this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+			this._firstConnection = false;
 		}
 
-        //standard webcompnent ; Fired when the widget is added to the html DOM of the page
-        connectedCallback(){
-        }
+		//Fired when the widget is added to the html DOM of the page
+		connectedCallback() {
+			this._firstConnection = true;
+			this.redraw();
+		}
 
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback(){
-        
-        }
+		//Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+		disconnectedCallback() {
 
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		}
+
+		//When the custom widget is updated, the Custom Widget SDK framework executes this function first
 		onCustomWidgetBeforeUpdate(oChangedProperties) {
 
 		}
 
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		//When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-            this.redraw();
-        }
-        
-        //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy(){
-        }
+			if (this._firstConnection) {
+				this.redraw();
+			}
+		}
 
-        
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default
-        /*
-        onCustomWidgetResize(width, height){
-        }
-        */
-        /*this method is not a custom widget framework callback. included a stub to this method in the template,  
-        There are many callbacks that could trigger re-rendering; 
-        so to centralise the redraw in a single method and call that method 
-        whenever need to update the widget in the canvas. 
-        */
-        redraw(){}
-    });
+		//When the custom widget is removed from the canvas or the analytic application is closed
+		onCustomWidgetDestroy() {}
+
+		//When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+		// Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+		//  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+		/*
+		onCustomWidgetResize(width, height){
+		    redraw()
+		}
+		*/
+
+		redraw() {}
+	});
 })();
